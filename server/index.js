fd38ef29b61cdafafdc08ad68363066723ac0b84
Niraj kumar chaurasiya <87218979+nirajkumar1234@users.noreply.github.com>
2022-12-26 20:57:39 +0545

    first commit

---
+++ server/index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+require('dotenv').config();
+const cors = require('cors')
+const { Configuration, OpenAIApi } = require('openai')
+
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+    res.status(200).send({
+        message: 'Hello from CodeX!'
+    });
+});
+
+app.post('/', async (req, res) => {
+    try {
+        const prompt = req.body.prompt;
+
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `${prompt}`,
+            temperature: 0.9,
+            max_tokens: 3000,
+            top_p: 1,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.6,
+            stop: [" Human:", " AI:"],
+        });
+
+        res.status(200).send({
+            bot: response.data.choices[0].text
+        });
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).send(error || 'Something went wrong');
+    }
+})
+
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
